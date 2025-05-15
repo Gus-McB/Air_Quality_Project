1f1d18a5adb9837f5dd6f8b27bc74806fdd6bbaf
Mcbac001 <mcbac001@mymail.unisa.edu.au>
2025-05-15 23:06:13 +0930

    normalised the data in preprocessing

---
+++ modules/preprocess.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import StandardScaler
+
+def preprocess(df):
+    df = df.dropna(subset=['Longitude','Latitude','AQI Value','AQI Category','CO AQI Value','CO AQI Category','Ozone AQI Value','Ozone AQI Category','NO2 AQI Value','NO2 AQI Category','PM2.5 AQI Value','PM2.5 AQI Category'])
+    features = ['AQI Value','AQI Category','CO AQI Value','CO AQI Category','Ozone AQI Value','Ozone AQI Category','NO2 AQI Value','NO2 AQI Category','PM2.5 AQI Value','PM2.5 AQI Category']
+    scaler = StandardScaler()
+    df[features] = scaler.fit_transform(df[features])
+    return df, df[features]
