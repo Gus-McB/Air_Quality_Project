9fc6b0e72c58c720b2df1c882f5a143e93623d60
Mcbac001 <mcbac001@mymail.unisa.edu.au>
2025-05-18 19:53:18 +0930

    made it save to cache each iteration

--- modules/geocode.py
+++ modules/geocode.py
@@ -2,27 +2,30 @@ import pandas as pd
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 
-def geocode_locations(df, cityCol='City', countryCol='Country', cache_path='../cache/geocodedCache.csv'):
+def geocodeLocations(df, cityCol='City', countryCol='Country', cachePath='../cache/geocodedCache.csv'):
     geolocator = Nominatim(user_agent="airQualityMapper")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
     try:
-        cache = pd.read_csv(cache_path)
-    except:
+        cache = pd.read_csv(cachePath)
+    except FileNotFoundError:
         cache = pd.DataFrame(columns=[cityCol, countryCol, 'Latitude', 'Longitude'])
     
     merged = df[[cityCol, countryCol]].drop_duplicates()
-    merged = merged.merge(columns=[cityCol, countryCol, 'Latitude', 'Longitude'])
+    merged = merged.merge(cache, on=[cityCol, countryCol], how='left')
 
     missing = merged[merged['Latitude'].isna()]
     for i, row in missing.iterrows():
-        location = geocode(f"{row[cityCol]}, {row[countryCol]}")
-        if location:
-            merged.at[i, 'Latitude'] = location.latitude
-            merged.at[i, 'Longitude'] = location.longitude
+        try:
+            location = geocode(f"{row[cityCol]}, {row[countryCol]}")
+            if location:
+                merged.at[i, 'Latitude'] = location.latitude
+                merged.at[i, 'Longitude'] = location.longitude
+                merged.dropna(subset=['Latitude', 'Longitude']).to_csv(cachePath, index=False)
+        except Exception as e:
+            print(f"Error geocoding {row[cityCol]}, {row[countryCol]}: {e}")
+
     
     updatedCache = merged.dropna(subset=['Latitude', 'Longitude'])
-    updatedCache.to_csv(cache_path, index=False)
-
     df = df.merge(updatedCache, on=[cityCol, countryCol], how='left')
     return df
