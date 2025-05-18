a383b0750961bbde35617746df83aa706eeaf85f
Mcbac001 <mcbac001@mymail.unisa.edu.au>
2025-05-18 11:11:19 +0930

    finished network and dbscan

--- modules/network.py
+++ modules/network.py
@@ -2,24 +2,6 @@ from keras.api.models import Model
 from keras.api.layers import Input, Dense
 from sklearn.cluster import DBSCAN
 
-
-numClass = len(label.classes_) #For the AQI categories 
-
-model = Model.Sequential([
-    layers.Dense(64, activation='relu', input_shape=(x.shape[1],)),
-    layers.Dense(32, activation='relu'),
-    layers.Dense(numClass, activation='softmax')
-])
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-training = model.fit(x, y, epochs=20, batch_size=32, validation_split=0.2)
-
-loss, accuracy = model.evaluate(x, y)
-print(f"Loss: {loss}, Accuracy: {accuracy}")
-predict = model.predict(x)
-
-
 def autoencoderModel(X, encoding_dim=2):
     inputLayer = Input(shape=(X.shape[1],))
     encoded = Dense(8, activation='relu')(inputLayer)
